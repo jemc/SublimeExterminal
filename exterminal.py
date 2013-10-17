e21e6e457652d1b9b86c3c631d7a37fd8c57ab21
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-17 03:28:34 -0400

    Added command to start a new Exterminal window

--- exterminal.py
+++ exterminal.py
@@ -1,5 +1,7 @@
 import sublime, sublime_plugin
 
+import os
+
 
 def wrapped_exec(self, *args, **kwargs):
     settings = sublime.load_settings("SublimeExterminal.sublime-settings")
@@ -12,7 +14,7 @@ def wrapped_exec(self, *args, **kwargs):
         
         kwargs['shell_cmd'] = shell_cmd
     
-    return self.run_cached_by_exterminal(self, *args, **kwargs)
+    return self.run_cached_by_exterminal(*args, **kwargs)
 
 def plugin_loaded():
     exec_cls = [cls for cls in sublime_plugin.window_command_classes \
@@ -24,3 +26,10 @@ def plugin_loaded():
     
     exec_cls.run_cached_by_exterminal = exec_cls.run
     exec_cls.run = wrapped_exec
+
+
+class StartExterminalCommand(sublime_plugin.WindowCommand):
+    def run(self, *args):
+        settings = sublime.load_settings("SublimeExterminal.sublime-settings")
+        cmd = settings.get('start_exterminal', '')
+        os.popen(cmd)
