bdb93cd4bc1befe99db4106d1f94243416b59d47
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-16 23:25:52 -0400

    Moved main methods back to outside of class to avoid confusion - they were never actually used in an object method context

--- exterminal.py
+++ exterminal.py
@@ -1,32 +1,26 @@
 import sublime, sublime_plugin
 
 
+def wrapped_exec(self, *args, **kwargs):
+    settings = sublime.load_settings("SublimeExterminal.sublime-settings")
+    
+    if settings.get('enabled') and kwargs.get('use_exterminal', True):
+        wrapper = settings.get('exec_wrapper')
+        
+        shell_cmd = kwargs.get('shell_cmd')
+        shell_cmd = wrapper % shell_cmd.replace('"','\\"')
+        
+        kwargs['shell_cmd'] = shell_cmd
+    
+    return self.run_cached_by_exterminal(self, *args, **kwargs)
+
 def plugin_loaded():
-    Exterminal.plugin_loaded()
-
-
-class Exterminal:
+    exec_cls = [cls for cls in sublime_plugin.window_command_classes \
+                    if cls.__qualname__=='ExecCommand'][0]
     
-    def wrapped_exec(self, *args, **kwargs):
-        settings = sublime.load_settings("SublimeExterminal.sublime-settings")
-        
-        if settings.get('enabled') and kwargs.get('use_exterminal', True):
-            wrapper = settings.get('exec_wrapper')
-            
-            shell_cmd = kwargs.get('shell_cmd')
-            shell_cmd = wrapper % shell_cmd.replace('"','\\"')
-            
-            kwargs['shell_cmd'] = shell_cmd
-        
-        return self.run_cached_by_exterminal(self, *args, **kwargs)
+    if hasattr(exec_cls(None), 'run_cached_by_exterminal'):
+        exec_cls.run = exec_cls.run_cached_by_exterminal
+        exec_cls.run_cached_by_exterminal = None
     
-    def plugin_loaded():
-        exec_cls = [cls for cls in sublime_plugin.window_command_classes \
-                        if cls.__qualname__=='ExecCommand'][0]
-        
-        if hasattr(exec_cls(None), 'run_cached_by_exterminal'):
-            exec_cls.run = exec_cls.run_cached_by_exterminal
-            exec_cls.run_cached_by_exterminal = None
-        
-        exec_cls.run_cached_by_exterminal = exec_cls.run
-        exec_cls.run = Exterminal.wrapped_exec
+    exec_cls.run_cached_by_exterminal = exec_cls.run
+    exec_cls.run = wrapped_exec
