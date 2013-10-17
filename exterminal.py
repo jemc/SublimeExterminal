7bc36c20d9b0df90456fbdfe4a919401148e9ae7
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-16 22:28:35 -0400

    Moved plugin methods to inside of a class

--- exterminal.py
+++ exterminal.py
@@ -2,14 +2,12 @@ import sublime, sublime_plugin
 
 
 def plugin_loaded():
-    exec_cls = [cls for cls in sublime_plugin.window_command_classes \
-                    if cls.__qualname__=='ExecCommand'][0]
+    Exterminal.plugin_loaded()
+
+
+class Exterminal:
     
-    if hasattr(exec_cls(None), 'run_cached_by_exterminal'):
-        exec_cls.run = exec_cls.run_cached_by_exterminal
-        exec_cls.run_cached_by_exterminal = None
-    
-    def my_exec(self, *args, **kwargs):
+    def wrapped_exec(self, *args, **kwargs):
         
         shell_cmd = kwargs.get('shell_cmd')
         shell_cmd = "echo \"%s\" > /tmp/sublpipe" % shell_cmd.replace('"','\\"')
@@ -17,5 +15,13 @@ def plugin_loaded():
         
         return self.run_cached_by_exterminal(self, *args, **kwargs)
     
-    exec_cls.run_cached_by_exterminal = exec_cls.run
-    exec_cls.run = my_exec
+    def plugin_loaded():
+        exec_cls = [cls for cls in sublime_plugin.window_command_classes \
+                        if cls.__qualname__=='ExecCommand'][0]
+        
+        if hasattr(exec_cls(None), 'run_cached_by_exterminal'):
+            exec_cls.run = exec_cls.run_cached_by_exterminal
+            exec_cls.run_cached_by_exterminal = None
+        
+        exec_cls.run_cached_by_exterminal = exec_cls.run
+        exec_cls.run = Exterminal.wrapped_exec
