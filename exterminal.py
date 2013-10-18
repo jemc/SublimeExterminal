9f0fdd5b2278eaf59e53abe8e53711ae409f5929
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-18 14:26:10 -0400

    Added support for cmd instead of shell_cmd

--- exterminal.py
+++ exterminal.py
@@ -9,10 +9,17 @@ def wrapped_exec(self, *args, **kwargs):
     if settings.get('enabled') and kwargs.get('use_exterminal', True):
         wrapper = settings.get('exec_wrapper')
         
-        shell_cmd = kwargs.get('shell_cmd')
-        shell_cmd = wrapper % shell_cmd.replace('"','\\"')
+        try:
+            shell_cmd = kwargs.get('shell_cmd', '')
+            shell_cmd = wrapper % shell_cmd.replace('"','\\"')
+            kwargs['shell_cmd'] = shell_cmd
+        except KeyError: pass
         
-        kwargs['shell_cmd'] = shell_cmd
+        try:
+            cmd = ' '.join(kwargs.get('cmd'))
+            kwargs['shell_cmd'] = wrapper % cmd.replace('"','\\"')
+        except KeyError: pass
+        
     
     return self.run_cached_by_exterminal(*args, **kwargs)
 
